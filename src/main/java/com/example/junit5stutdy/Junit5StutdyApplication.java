2dc104be817db2f0f437968edfc1747f99a7749d
kyubeom <be_man@naver.com>
2022-11-11 22:53:23 +0900

    initial commit

---
+++ src/main/java/com/example/junit5stutdy/Junit5StutdyApplication.java
@@ -0,0 +1,13 @@
+package com.example.junit5stutdy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5StutdyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Junit5StutdyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/junit5stutdy/Junit5StutdyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junit5stutdy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5StutdyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
